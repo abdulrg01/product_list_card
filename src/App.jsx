2bedd37e05e5fd73351a0034104c207de8a96dfa
abdulrg01 <abdulrg01@gmail.com>
2024-08-11 11:52:26 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import CardPage from "./components/CardPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <CardPage />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="flex items-center justify-center text-black gap-1">
+      <a href="https://www.frontendmentor.io/challenges" target="_blank">
+        Frontend Mentor Challenge coded
+      </a>
+      <a href="https://abdul-navy.vercel.app" target="_blank" className="underline">
+        <strong>by Abdulrahman Abubakar</strong>
+      </a>
+    </div>
+  );
+}
